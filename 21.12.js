554cbeac38fd5ecd54e6ac0447a96d2d21dba28e
Gaabuto <khanh040604@gmail.com>
2025-03-05 16:31:07 +0700

    Add files via upload

---
+++ 21.12.js
@@ -0,0 +1,7 @@
+for (let i = 0; i < 10; i++) {
+    let color = ['red', 'orange', 'green', 'blue', 'pink',];
+    let random = color[Math.floor(Math.random() * color.length)];
+    console.log('Hello World', 'color: ' + random);
+
+
+}
---
+++ 21.13.js
@@ -0,0 +1,52 @@
+console.log('a.');
+console.log('********');
+console.log('*******');
+console.log('******');
+console.log('*****');
+console.log('****');
+console.log('***');
+console.log('**');
+console.log('*');
+
+
+
+
+
+
+
+
+console.log('b.');
+console.log('*');
+console.log('**');
+console.log('***');
+console.log('****');
+console.log('*****');
+console.log('******');
+console.log('*******');
+console.log('********');
+
+console.log('c.');
+console.log('       *');
+console.log('      **');
+console.log('     ***');
+console.log('    ****');
+console.log('   *****');
+console.log('  ******');
+console.log(' *******');
+console.log('********');
+
+console.log('d.');
+console.log('********');
+console.log(' *******');
+console.log('  ******');
+console.log('   *****');
+console.log('    ****');
+console.log('     ***');
+console.log('      **');
+console.log('       *');
+
+
+
+
+
+
